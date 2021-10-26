861eb73021a4c0e8c850dbb64cadd72b86ca30bd
BarbonFisso <barbieri.mat@hotmail.it>
2021-10-26 14:49:39 +0200

    aggiunto commento programma js mail

--- mail/js/mail_script.js
+++ mail/js/mail_script.js
@@ -1,3 +1,8 @@
 // Mail
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere(che hai fatto tu precedentemente), stampa un messaggio appropriato sull’esito del controllo.
 // Qui, come detto, NON possiamo usare metodi nuovi sugli array ma lo facciamo con le cose viste questa mattina.
+
+// creo un array vuoto e genero mail casuali da aggiungere come elementi
+// chiedo all'utente la sua mail
+// confronto la mail dell'utente con gli elementi dell'array 
+// se trovo una corrispondenza ammetto l'utente, altrimenti gli nego l'accesso
