a650b9ae26fa9bc57fa626200569c4a2a4e93efc
BarbonFisso <barbieri.mat@hotmail.it>
2021-10-26 14:32:27 +0200

    terminato esercizio dadi

--- dadi/js/dadi_script.js
+++ dadi/js/dadi_script.js
@@ -2,9 +2,28 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-// genero 2 numeri casuali interi tra 1 e 6 e li stampo
+// genero 2 numeri casuali interi tra 1 e 6 e li 
 // uno sarà assegnato all'utente e l'altro al computer
+const risultati = document.getElementById("container");
+
+const userNum = Math.floor(Math.random() * 6) + 1;
+console.log("il numero dell'utente è ", userNum);
+risultati.innerHTML += `Il tuo numero è ${userNum} <br>`;
+
+const pcNum = Math.floor(Math.random() * 6) + 1;
+console.log("il numero del computer è ", pcNum);
+risultati.innerHTML += `Il numero del computer è ${pcNum} <br>`;
+
 // confronto i due numeri
 // il più alto sarà il vincitore
-// stampo il nome di chi ha vinto 
-
+// stampo il nome di chi ha vinto o se è un pareggio
+if (userNum === pcNum) {
+    console.log("È un pareggio");
+    risultati.innerHTML += `È un pareggio.`;
+} else if (userNum > pcNum) {
+    console.log("L'utente ha vinto");
+    risultati.innerHTML += `Hai vinto!`;
+} else {
+    console.log("Il computer ha vinto");
+    risultati.innerHTML += `Il computer ha vinto. Ritenta aggiornando la pagina.`;
+}
