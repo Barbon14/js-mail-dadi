19a3f0c591c3eb1c1508816a05b6a0f78676c641
BarbonFisso <barbieri.mat@hotmail.it>
2021-10-26 14:11:19 +0200

    aggiunto analisi prog al js dadi

--- dadi/js/dadi_script.js
+++ dadi/js/dadi_script.js
@@ -1,3 +1,10 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// genero 2 numeri casuali interi tra 1 e 6 e li stampo
+// uno sarà assegnato all'utente e l'altro al computer
+// confronto i due numeri
+// il più alto sarà il vincitore
+// stampo il nome di chi ha vinto 
+
